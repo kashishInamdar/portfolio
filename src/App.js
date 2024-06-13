188683f0a8760e5f54d5308f49cce22f8286c6e2
kashishInamdar <kashishinamdar2004@gmail.com>
2024-06-13 21:42:22 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import About from "./component/About";
-import Contact from "./component/Contact";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
+// import About from "./component/About";
+// import Contact from "./component/Contact";
+// import Footer from "./component/Footer";
+// import Header from "./component/Header";
 import Navbar from "./component/Navbar";
-import Project from "./component/Project";
-import Services from "./component/Services";
-import Skill from "./component/Skill";
-import Testimonials from "./component/Testimonials";
+// import Project from "./component/Project";
+// import Services from "./component/Services";
+// import Skill from "./component/Skill";
+// import Testimonials from "./component/Testimonials";
+import "./App.css"
 
 function App() {
   return (
     <>
      <Navbar />
-     <Header/>
+     {/* <Header/>
      <About/>
      <Services />
      <Skill />
      <Project />
      <Testimonials />
      <Contact />
-     <Footer />
+     <Footer /> */}
     </>
   );
 }
--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,11 +1,58 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-const Navbar = ()=>{
-    return(
-        <div>
-            <h1>This in Navebar</h1>
+import { SiWebmoney } from "react-icons/si";
+import { menu } from "./../../data";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { FaArrowUpRightFromSquare, FaBarsStaggered } from "react-icons/fa6";
+import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+
+
+const Navbar = () => {
+
+  const [showSidebar , setShowSidebar] = useState(false)
+  return (
+    <nav>
+      <div className="navbar__container">
+
+       {
+        showSidebar ? (
+          <div className="overly" onClick={()=>{setShowSidebar(!showSidebar)}} ></div>
+        ): ""
+
+       }
+
+        <div className="log__container">  
+          <SiWebmoney  />
         </div>
-    )
-}
 
-export default Navbar ;
+        <div className={`tab__group ${showSidebar ? "show" : ""}`}>
+          <span className="icon__container close__btn" onClick={()=>{setShowSidebar(!showSidebar)}} >
+            <FaTimes />
+          </span>
+          {menu.map((list, index) => (
+            <Link
+              activeClass="active"
+              className="tab__item name"
+              to={list.name.toLowerCase()}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={index}>
+              {list.name}
+            </Link>
+          ))}
+        </div>
+        <div className="nav__button__groups">
+          <button className="btn btn_primary">
+            Hear Me <FaArrowUpRightFromSquare />{" "}
+          </button>
+          <FaBarsStaggered className="menu"  onClick={()=>{setShowSidebar(!showSidebar)}}/>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
